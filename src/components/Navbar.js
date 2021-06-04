7e9c68a9da28e77b3494df9d0b8fd472dcb5421e
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-06-04 10:48:39 +0100

    added react router

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<>
+			<div className="navbar">
+				<div className="navabr-container container">
+					<Link className="navbar-logo">Everything Audio</Link>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Navbar;
