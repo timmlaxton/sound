b6ac493d15f61963813f7086cffbcdd5ff371d67
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-06-04 11:38:47 +0100

    added button css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-	return <div className="App">Hello</div>;
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path="/" />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiHeadphones } from 'react-icons/fi';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Button } from '../components/Button';
 
 const Navbar = () => {
+	const [click, setClick] = useState(false);
+	const [button, setButton] = useState(true);
+
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+
+	const showButton = () => {
+		if (window.innerWidth <= 960) {
+			setButton(false);
+		} else {
+			setButton(true);
+		}
+	};
+
+	window.addEventListener('resize', showButton);
+
 	return (
 		<>
 			<div className="navbar">
 				<div className="navabr-container container">
-					<Link className="navbar-logo">Everything Audio</Link>
+					<Link to="/" className="navbar-logo">
+						<FiHeadphones className="navbar-icon" />
+						Everything Audio
+					</Link>
+					<div className="menu-icon" onClick={handleClick}>
+						{click ? <FaTimes /> : <FaBars />}
+					</div>
+					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+						<li className="nav-item">
+							<Link to="/" className="nav-links">
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/" className="nav-links">
+								Equipment
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/" className="nav-links">
+								Booking & Rates
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/" className="nav-links">
+								Contact
+							</Link>
+						</li>
+						<li className="nav-btn">
+							{button ? (
+								<Link to="/sign-up" className="btn-link">
+									<Button buttonStyle="btn--outlin">Sign Up</Button>
+								</Link>
+							) : (
+								<Link to="/sign-up" className="btn-link">
+									<Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+										Sign Up
+									</Button>
+								</Link>
+							)}
+						</li>
+					</ul>
 				</div>
 			</div>
 		</>
