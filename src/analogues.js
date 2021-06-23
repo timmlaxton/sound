176d91e024e9027c158503b9e38e73cb7661214b
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-06-23 09:47:33 +0100

    added analogue descriptions

--- src/analogues.js
+++ src/analogues.js
@@ -3,7 +3,8 @@ const analogues = [
 		_id: '1',
 		name: 'Warm Audio WA-412',
 		image: '/images/analogues/Warm Audio WA-412.jpg',
-		description: '8 channels',
+		description:
+			'Housed in a 1U 19-inch rackmounting enclosure, the WA-412  includes 48V phantom power, polarity inversion, a 20dB pad, and a high-impedance instrument input.',
 		category: 'analogue',
 		quantity: 2
 	},
@@ -11,7 +12,8 @@ const analogues = [
 		_id: '2',
 		name: 'Warm Audio WA-273',
 		image: '/images/analogues/Warm Audio WA-273.jpg',
-		description: '2 channels',
+		description:
+			'The WA73 delivers up to 80dB of gain and includes custom Carnhill transformers specifically designed to recreate the sweet saturation and vintage tone of the original. Tantalum and polystyrene capacitors are also in use in the signal path providing smoother performance at high gain settings than any other ‘73 clone.',
 		category: 'analogue',
 		quantity: 1
 	},
@@ -19,15 +21,17 @@ const analogues = [
 		_id: '3',
 		name: 'Golden Age Project Pre73',
 		image: '/images/analogues/golden age project pre73.jpg',
-		description: '2 channels',
+		description:
+			'The PRE-73 MKIII is a one-channel vintage style microphone-, line- and instrument preamplifier. The signal path uses only discrete components like resistors, capacitors and transistors. The in- and output is transformer balanced, using two different transformers, each one optimized for its purpose. This is the way audio components were built before integrated circuits became available.',
 		category: 'analogue',
 		quantity: 2
 	},
 	{
 		_id: '4',
-		name: 'FMR Really Nice Preamp',
+		name: 'FMR Audio RNP 8380',
 		image: '/images/analogues/FMR Really Nice Preamp.jpg',
-		description: '2 channels',
+		description:
+			'The FMR Really Nice Preamp RNP8380 is a pair of separate mic preamps providing up to 66dB of gain at astonishingly low distortion levels in a compact 1/3 rack format.',
 		category: 'analogue',
 		quantity: 2
 	},
@@ -36,14 +40,16 @@ const analogues = [
 		name: 'FMR Really Nice Compressor',
 		image: '/images/analogues/FMR Really Nice Compressor.jpg',
 		category: 'analogue',
-		description: 'stereo',
+		description:
+			'The RNC is a compact which provides two modes of operation: normal and SuperNice. Normal mode allows the RNC to be used for everyday compression tasks, like punchy snare drums or bass guitars, and is great for everything from gentle compression to serious squashing. SuperNice mode provides near-invisible signal compression for sensitive material where compression artifacts are less desirable (like vocals, acoustic guitar or the program bus). The RNC yields clean, airy compression with few artifacts and little high end loss, even at massive gain reduction.',
 		quantity: 1
 	},
 	{
 		_id: '7',
 		name: 'DBX 160XT',
 		image: '/images/analogues/DBX 160XT.jpg',
-		description: '2 channels mono, dual mono or stereo linked',
+		description:
+			'The 160XT is a drum compressor which adds punch & attitude to kicks & snares and is excellent for squashing overheads and ambience mics.',
 		category: 'analogue',
 		quantity: 2
 	},
@@ -51,7 +57,8 @@ const analogues = [
 		_id: '8',
 		name: 'SPL Vitalizer',
 		image: '/images/analogues/SPL Vitalizer.jpg',
-		description: 'stereo',
+		description:
+			'Armed with precise filtering functions and an intuitive usability, the Stereo Vitalizer Mk2-T can empower productions with a more stimulating and natural timbre almost instantly.',
 		category: 'analogue',
 		quantity: 1
 	}
