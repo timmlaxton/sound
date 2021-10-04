4ea8418487dd459d5923e87e8ecc5591b084394d
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-10-04 11:47:18 +0100

    email response

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -7,9 +7,12 @@ const Contact = () => {
   const [state, handleSubmit] = useForm("xeqvvody");
   if (state.succeeded) {
     return (
-      <p className="emailresponse">
-        Thanks for gettting in touch, I will be in contact to discuss further.
-      </p>
+      <>
+        <h1>Thanks for the email</h1>
+        <p className="emailresponse">
+          Thanks for gettting in touch, I will be in contact to discuss further.
+        </p>
+      </>
     );
   }
   return (
