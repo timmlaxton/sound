99600c2330aef15d635aab2e1edc11d150f40770
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-10-04 11:37:24 +0100

    reformatted contact form

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,79 +1,117 @@
-import React, { useState } from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import './Contact.css';
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import "./Contact.css";
 
 const Contact = () => {
-	const [status, setStatus] = useState('Submit');
+  const [state, handleSubmit] = useForm("xeqvvody");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+  return (
+    <Container className="my-4">
+      <Row>
+        <Col>
+          <div className="contact-container">
+            <div className="contact-paragraph">
+              {" "}
+              <p>
+                {" "}
+                Sound engineer working in the live sector as a freelancer and
+                chief engineer at Bloc+, Glasgow. My aim is to move into the
+                sound design sector with a view to enhancing interactive audio
+                integration into theatre, dance and therapeutic applications.{" "}
+              </p>
+              <br />
+              <p> For Rates & Inquiries Fill In The Form Below </p>{" "}
+            </div>
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setStatus('Sending...');
-		const { name, email, message } = e.target.elements;
-		let details = {
-			name: name.value,
-			email: email.value,
-			message: message.value
-		};
-		let baseUrl = process.env.NODE_ENV === 'development' ? '' : window.location.origin;
-		let response = await fetch(`${baseUrl}/api/contact`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8'
-			},
-			body: JSON.stringify(details)
-		});
-		setStatus('Submit');
-		let result = await response.json();
-		alert(result.status);
-	};
+            <Row>
+              <Col md={4} className="mx-auto">
+                <form onSubmit={handleSubmit}>
+                  <Form.Group>
+                    <Form.Label htmlFor="name">Name</Form.Label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="text"
+                      placeholder="name*"
+                      name="name"
+                    />
+                  </Form.Group>
 
-	return (
-		<Container className="my-4">
-			<Row>
-				<Col>
-					<div className="contact-container">
-						<div className="contact-paragraph">
-							{' '}
-							<p>
-								{' '}
-								Sound engineer working in the live sector as a freelancer and chief engineer at Bloc+, Glasgow. My aim
-								is to move into the sound design sector with a view to enhancing interactive audio integration into
-								theatre, dance and therapeutic applications.{' '}
-							</p>
-							<br />
-							<p> For Rates & Inquiries Fill In The Form Below </p>{' '}
-						</div>
+                  <ValidationError
+                    prefix="Name"
+                    field="name"
+                    errors={state.errors}
+                  />
 
-						<Row>
-							<Col md={6} className="mx-auto">
-								<Form onSubmit={handleSubmit}>
-									<Form.Group className="form-name">
-										<Form.Label htmlFor="name">Name: {''}</Form.Label>
-										<Form.Control type="text" className="form-control" id="name" placeholder="name*" />
-									</Form.Group>
-									<Form.Group className="form">
-										<Form.Label htmlFor="exampleFormControlInput1">Email: {''}</Form.Label>
-										<Form.Control type="email" className="form-control" id="email" placeholder="email*" />
-									</Form.Group>
-									<Form.Group className="form">
-										<Form.Label htmlFor="exampleFormControlInput1">City: {''}</Form.Label>
-										<Form.Control type="text" className="form-control" id="city" placeholder="city*" />
-									</Form.Group>
-									<Form.Group className="form-message">
-										<Form.Label htmlFor="message">Message: {''}</Form.Label>
-										<Form.Control as="textarea" className="form" id="message" rows="10" placeholder="message*" />
-									</Form.Group>
-									<Button variant="primary" type="submit">
-										{status}
-									</Button>
-								</Form>
-							</Col>
-						</Row>
-					</div>
-				</Col>
-			</Row>
-		</Container>
-	);
+                  <Form.Group>
+                    <Form.Label htmlFor="email">Email Address</Form.Label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="email*"
+                      name="email"
+                    />
+                  </Form.Group>
+
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                  />
+
+                  <Form.Group>
+                    <Form.Label htmlFor="city">City</Form.Label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="city"
+                      placeholder="city*"
+                      name="city"
+                    />
+                  </Form.Group>
+
+                  <ValidationError
+                    prefix="City"
+                    field="city"
+                    errors={state.errors}
+                  />
+
+                  <Form.Group className="form-message">
+                    <Form.Label htmlFor="message">Message: {""}</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      className="form"
+                      id="message"
+                      rows="10"
+                      placeholder="message*"
+                      name="message"
+                    />
+                  </Form.Group>
+
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                  />
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    disabled={state.submitting}
+                  >
+                    Submit
+                  </Button>
+                </form>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Contact;
